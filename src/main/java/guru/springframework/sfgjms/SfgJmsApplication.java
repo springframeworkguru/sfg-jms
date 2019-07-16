3227141eff2cb23c62739deb01f37035590bc901
John Thompson <john@springframework.guru>
2019-07-16 18:41:20 -0400

    initial project add

---
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJmsApplication.class, args);
+	}
+
+}
