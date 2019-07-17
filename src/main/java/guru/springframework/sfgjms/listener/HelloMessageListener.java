9efb43d8291ec78f85da0fb8cd214d057f882d47
John Thompson <john@springframework.guru>
2019-07-17 18:05:43 -0400

    adding message listener

---
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgjms.listener;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+/**
+ * Created by jt on 2019-07-17.
+ */
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message){
+
+        System.out.println("I Got a Message!!!!!");
+
+        System.out.println(helloWorldMessage);
+
+
+        // uncomment and view to see retry count in debugger
+       // throw new RuntimeException("foo");
+
+    }
+
+}
