e3c7678b2e1e059362e49f1e826fe6a747f52d35
John Thompson <john@springframework.guru>
2019-07-19 18:03:45 -0400

    config for use with local broker

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,8 +1,6 @@
 package guru.springframework.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +9,13 @@ public class SfgJmsApplication {
 
 	public static void main(String[] args) throws Exception {
 
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-			.setPersistenceEnabled(false)
-			.setJournalDirectory("target/data/journal")
-			.setSecurityEnabled(false)
-			.addAcceptorConfiguration("invm", "vm://0"));
-
-		server.start();
+//		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//			.setPersistenceEnabled(false)
+//			.setJournalDirectory("target/data/journal")
+//			.setSecurityEnabled(false)
+//			.addAcceptorConfiguration("invm", "vm://0"));
+//
+//		server.start();
 
 		SpringApplication.run(SfgJmsApplication.class, args);
 	}
