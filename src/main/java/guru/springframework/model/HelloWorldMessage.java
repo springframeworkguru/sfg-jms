1bfa8099225af1b7ba67d7457bcd1abf5607f829
JakuBB96 <JakuBB96@javadev.com>
2020-04-17 19:23:31 +0200

    Setting up basic server Configuring task executor Setting up simple pojo

---
+++ src/main/java/guru/springframework/model/HelloWorldMessage.java
@@ -0,0 +1,20 @@
+package guru.springframework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    private UUID id;
+    private String message;
+    private static final long serialVersionUID = -2083071851260941907L;
+}
